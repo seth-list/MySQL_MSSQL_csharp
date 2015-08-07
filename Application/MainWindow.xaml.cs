67a354a1bc07468dcd2faa4a7d294ca56868eef0
seth-list <seth_t2@mail.ru>
2015-08-07 20:53:26 +0600

    Added classes

---
+++ Application/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private SQL db; //abstract db
+        //private int indDb = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cb1.Items.Add("MySQL");
+            cb1.Items.Add("MSSQL");
+
+            
+            cb1.SelectedIndex = 0;
+            cb2.SelectedIndex = 0;
+            cb3.SelectedIndex = 0;
+            //инициализация раскрывающегося списка с базами данных
+            cb1_init();
+            //инициализация раскрывающегося списка с таблицами
+            cb2_init();
+        }
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cb2.Items.Count == 0 || cb3.Items.Count == 0)
+                return;
+
+            String sdb = cb2.Text;
+            String stb = cb3.Text;
+            
+            List<string> cols = db.getAllColumns(sdb, stb);
+            DataTable dt = new DataTable();
+
+            //очистка данных
+            lstw.Columns.Clear();
+
+            //TODO доделать
+            foreach (string data in cols)
+            {
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                textColumn.Header = data;                
+                textColumn.Binding = new Binding(string.Format("[{0}]", data));
+                lstw.Columns.Add(textColumn);
+                //data table
+                dt.Columns.Add(data);
+            }
+            
+            List<List<string>> sds = db.getDataOfTable(sdb, stb);
+            foreach (List<string> sd in sds)
+            {
+                dt.Rows.Add(sd.ToArray());
+            }
+
+            //context
+            lstw.DataContext = dt;
+
+
+        }
+
+
+        private void cb1_DropDownClosed(object sender, EventArgs e)
+        {
+            cb1_init();
+            cb2_init();
+        }
+
+        private void cb2_DropDownClosed(object sender, EventArgs e)
+        {
+            cb2_init();
+        }
+
+
+
+        private void cb1_init()
+        {
+            String sdb = cb1.Text;
+            List<string> databs;
+            switch (sdb)
+            {
+                case "MySQL":
+                    db = new MySQL();
+                    databs = db.getAllDataBases();
+                    cb2.Items.Clear();
+
+                    foreach (string data in databs)
+                    {
+                        cb2.Items.Add(data);
+                    }
+                    cb2.SelectedIndex = 0;
+
+                    break;
+
+                case "MSSQL":
+                    db = new MSSQL();
+                    databs = db.getAllDataBases();
+
+                    cb2.Items.Clear();
+
+                    foreach (string data in databs)
+                    {
+                        cb2.Items.Add(data);
+                    }
+                    cb2.SelectedIndex = 0;
+
+                    break;
+            }
+        }
+
+        private void cb2_init()
+        {
+            
+            String sdb = cb2.Text;
+            List<string> tables = db.getAllTables(sdb);
+
+            cb3.Items.Clear();
+            foreach (string data in tables)
+            {
+                cb3.Items.Add(data);
+            }
+            cb3.SelectedIndex = 0;
+
+        }
+
+        
+
+
+    }
+}
+
---
+++ Application/SQL.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+
+
+namespace WpfApplication1
+{
+    //abstract class for any system implementation
+    public abstract class SQL
+    {
+
+        public abstract List<string> getAllDataBases();
+
+        public abstract List<string> getAllTables(String database);
+
+        public abstract List<string> getAllColumns(String database, String table);
+
+        public abstract List<List<string>> getDataOfTable(String database, String table);
+
+    }
+
+    public class MySQL : SQL
+    {
+        private MySqlConnection connection;
+        private string server = "localhost";
+        private string login = "root";
+        private string password = "123";
+
+        private void connect()
+        {
+            //mysql table
+            string myConnectionString = "SERVER=" + server + ";UID='" + login + "';"
+                + "PASSWORD='" + password + "';";
+
+            connection = new MySqlConnection(myConnectionString);
+            connection.Open();
+        }
+
+        private void disconnect()
+        {
+            connection.Close();
+        }
+
+
+        public override List<string> getAllDataBases()
+        {
+            connect();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SHOW DATABASES;";
+            //========================================================
+            MySqlDataReader Reader;
+            List<string> data = new List<string>();
+
+            try
+            {
+                Reader = command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    //добавление баз данных всех
+                    data.Add(Reader.GetValue(0).ToString());
+                }
+
+                disconnect();
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+
+            data.Remove("information_schema");
+            data.Remove("performance_schema");
+            data.Remove("mysql");
+
+
+            return data;
+        }
+
+
+        public override List<string> getAllTables(String database)
+        {
+
+            connect();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "show tables from " + database + ";";
+            //========================================================
+            MySqlDataReader Reader;
+
+            List<string> data = new List<string>();
+
+            try
+            {
+                Reader = command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    data.Add(Reader.GetValue(0).ToString());
+                }
+
+                disconnect();
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            return data;
+
+        }
+
+
+        public override List<string> getAllColumns(String database, String table)
+        {
+
+            connect();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = String.Format("SHOW COLUMNS FROM {0} FROM {1}",
+                table, database);
+            //========================================================
+            MySqlDataReader Reader;
+
+            List<string> data = new List<string>();
+
+            try
+            {
+                Reader = command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    data.Add(Reader.GetValue(0).ToString());
+                }
+
+                disconnect();
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            return data;
+
+
+        }
+
+
+        public override List<List<string>> getDataOfTable(String database, String table)
+        {
+            connect();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = String.Format("USE {0}; SELECT * FROM {1};",
+                database, table);
+            //========================================================
+            MySqlDataReader Reader;
+
+            List<List<string>> data = new List<List<string>>();
+
+            try
+            {
+                Reader = command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    List<string> row = new List<string>();
+
+                    for (int i = 0; i < Reader.FieldCount; i++)
+                    {
+                        row.Add(Reader.GetValue(i).ToString());
+                    }
+
+                    data.Add(row);
+                }
+
+                disconnect();
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            return data;
+        }
+    }
+
+    public class MSSQL : SQL
+    {
+
+        private SqlConnection sqlConnection;
+
+        private void connect()
+        {
+            //using windows authentiphication
+            //connetionString="Data Source=ServerName;
+            //Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            var connectionString = string.Format("Data Source=.\\SQLExpress;Integrated Security=SSPI;");
+
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public override List<string> getAllDataBases()
+        {
+
+            connect();
+            DataTable databases = null;
+
+            try
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    databases = sqlConnection.GetSchema("Databases");
+                    sqlConnection.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            List<string> data = new List<string>();
+            if (databases != null)
+            {
+                foreach (DataRow row in databases.Rows)
+                {
+                    data.Add(row.ItemArray[0].ToString());
+                }
+            }
+
+            data.Remove("master");
+            data.Remove("model");
+            data.Remove("msdb");
+            data.Remove("tempdb");
+
+            return data;
+
+        }
+
+        public override List<string> getAllTables(String database)
+        {
+
+            List<string> data = new List<string>();
+            DataTable table = null;
+
+            try
+            {
+                var connectionString = string.Format("Data Source=.\\SQLExpress;Integrated Security=SSPI;" +
+                    "Database=" + database + ";");
+
+                sqlConnection = new SqlConnection(connectionString);
+
+                try
+                {
+                    using (sqlConnection)
+                    {
+                        sqlConnection.Open();
+                        table = sqlConnection.GetSchema("Tables");
+                        sqlConnection.Close();
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    System.Windows.MessageBox.Show(ex.ToString());
+                }
+
+                if (table != null)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        //2 элемент это база данных
+                        data.Add(row.ItemArray[2].ToString());
+                    }
+                }
+                return data;
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+            return data;
+
+        }
+
+        public override List<string> getAllColumns(String database, String table)
+        {
+
+            List<string> data = new List<string>();
+
+            try
+            {
+                var connectionString = string.Format("Data Source=.\\SQLExpress;Integrated Security=SSPI;" +
+                    "Database=" + database + ";");
+
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+
+                try
+                {
+                    using (sqlConnection)
+                    {
+
+                        //Select * From DBNAME.INFORMATION_SCHEMA.COLUMNS
+                        SqlCommand command = sqlConnection.CreateCommand();
+                        command.CommandText = String.Format("SELECT COLUMN_NAME FROM "
+                            + "INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{0}' ", table);
+                        //========================================================
+                        SqlDataReader Reader;
+
+                        Reader = command.ExecuteReader();
+
+                        while (Reader.Read())
+                        {
+                            data.Add(Reader.GetValue(0).ToString());
+                        }
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    System.Windows.MessageBox.Show(ex.ToString());
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            return data;
+
+
+        }
+
+        public override List<List<string>> getDataOfTable(String database, String table)
+        {
+
+            List<List<string>> data = new List<List<string>>();
+
+            try
+            {
+                var connectionString = string.Format("Data Source=.\\SQLExpress;Integrated Security=SSPI;" +
+                    "Database=" + database + ";");
+
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+
+                try
+                {
+                    using (sqlConnection)
+                    {
+
+                        //Select * From DBNAME.INFORMATION_SCHEMA.COLUMNS
+                        SqlCommand command = sqlConnection.CreateCommand();
+                        command.CommandText = String.Format("SELECT * FROM {0} ", table);
+                        //========================================================
+                        SqlDataReader Reader;
+
+                        Reader = command.ExecuteReader();
+
+                        while (Reader.Read())
+                        {
+                            List<string> row = new List<string>();
+
+                            for (int i = 0; i < Reader.FieldCount; i++)
+                            {
+                                row.Add(Reader.GetValue(i).ToString());
+                            }
+
+                            data.Add(row);
+                        }
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    System.Windows.MessageBox.Show(ex.ToString());
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            return data;         
+        }
+    }
+
+
+
+}
